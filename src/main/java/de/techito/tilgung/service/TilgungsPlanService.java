50380c96d222586420c26f3682940a76a0e3875f
tnielsen57 <tim.nielsen91@googlemail.com>
2020-09-16 22:25:42 +0200

    Refactor percentage calculation for annual rates

--- src/main/java/de/techito/tilgung/service/TilgungsPlanService.java
+++ src/main/java/de/techito/tilgung/service/TilgungsPlanService.java
@@ -24,11 +24,15 @@ public class TilgungsPlanService {
 
         List<Zahlung> zahlungen = new ArrayList<>();
 
+        // Faktor für Berechnung der Tilgung / Zinsen aus Prozentwert ermitteln
+        zinsSatz = zinsSatz.movePointLeft(2);
+        tilgungsSatz = tilgungsSatz.movePointLeft(2);
+
         BigDecimal monatlicheRate;
         if (gewuenschterMonatsbeitrag != null && gewuenschterMonatsbeitrag.compareTo(BigDecimal.ZERO) != 0) {
             monatlicheRate = gewuenschterMonatsbeitrag;
         } else {
-            BigDecimal rateImErstenJahr = darlehensSumme.multiply(tilgungsSatz.divide(BigDecimal.valueOf(100))).add(darlehensSumme.multiply(zinsSatz));
+            BigDecimal rateImErstenJahr = darlehensSumme.multiply(tilgungsSatz).add(darlehensSumme.multiply(zinsSatz));
             monatlicheRate = rateImErstenJahr.divide(BigDecimal.valueOf(12), RoundingMode.HALF_UP);
         }
 
@@ -49,7 +53,7 @@ public class TilgungsPlanService {
             BigDecimal tilgungDerRate;
 
             if (restDarlehen.compareTo(monatlicheRate) > 0) {
-                zinsenDerRate = restDarlehen.multiply(zinsSatz.divide(BigDecimal.valueOf(100))).divide(BigDecimal.valueOf(12), 2, RoundingMode.HALF_UP);
+                zinsenDerRate = restDarlehen.multiply(zinsSatz).divide(BigDecimal.valueOf(12), 2, RoundingMode.HALF_UP);
                 tilgungDerRate = monatlicheRate.subtract(zinsenDerRate).setScale(2, RoundingMode.HALF_UP);
                 restDarlehen = restDarlehen.subtract(tilgungDerRate);
                 gesamtZinsen = gesamtZinsen.add(zinsenDerRate);
